b6ba624fd8f090e3fd5b91f86f8003b038e4f455
RheticusCompass <ctina.elizabeth@gmail.com>
2015-08-23 23:23:42 -0500

    added watch

--- gulpfile.js
+++ gulpfile.js
@@ -45,3 +45,11 @@ gulp.task('compass', function(){
 		.on('error', gutil.log)
 		.pipe(gulp.dest('builds/development/css'))
 });
+
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+
+gulp.task('watch', function(){
+	gulp.watch(coffeeSources, ['coffee']);
+	gulp.watch(jsSources, ['js']);
+	gulp.watch('components/sass/*.scss', ['compass']);
+});
